360eb8aee55ae0abf14ee836a5411cdab3ade891
Ashutosh Rajput <rajputashutosh81@gmail.com>
2025-07-08 14:42:26 +0530

    frist commit

---
+++ src/main/java/com/azure/emailbygraphapi/EmailByGraphApiApplication.java
@@ -0,0 +1,13 @@
+package com.azure.emailbygraphapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailByGraphApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailByGraphApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azure/emailbygraphapi/EmailByGraphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.emailbygraphapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailByGraphApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
